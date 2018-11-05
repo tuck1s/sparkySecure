9d567d7e36f83d2e2408c27b0e2282c0b1cfb3fd
tuck1s <steve.tuck@sparkpost.com>
2018-11-05 22:19:15 +0000

    Refactored sending code to allow output to stdout. Specific dependencies in Pipfile, README improvements.

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 
 [Sign up](https://app.sparkpost.com/join?plan=free-0817?src=Social%20Media&sfdcid=70160000000pqBb&pc=GitHubSignUp&utm_source=github&utm_medium=social-media&utm_campaign=github&utm_content=sign-up) for a SparkPost account and visit our [Developer Hub](https://developers.sparkpost.com) for even more content.
 
-
-# sparkpostSMIME
+# sparkySecure
 [![Build Status](https://travis-ci.org/tuck1s/sparkySecure.svg?branch=master)](https://travis-ci.org/tuck1s/sparkySecure)
 
+A collection of tools for working with secure S/MIME format files and SparkPost.
+
+# sparkpostSMIME
 Send an email file via SparkPost with optional S/MIME encryption and signing.
 
 ## Usage
-
 ```
-$ ./sparkpostSMIME.py -h
-usage: sparkpostSMIME.py [-h] [--encrypt] [--sign] emlfile
+$  ./sparkpostSMIME.py -h
+usage: sparkpostSMIME.py [-h] [--encrypt] [--sign] [--send_api] emlfile
 
 Send an email file via SparkPost with optional S/MIME encryption and signing.
 
@@ -26,6 +27,7 @@ optional arguments:
   --sign      Sign with a sender key. Requires file.crt containing public key,
               and file.pem containing private key, where file matches From:
               address.
+  --send_api  Send via SparkPost API (using env var SPARKPOST_API_KEY).
 ```
 
 When sending an .eml file from sender@example.com to recip@gmail.com, the following key files are required:
@@ -36,22 +38,44 @@ When sending an .eml file from sender@example.com to recip@gmail.com, the follow
 |`sender@example.com.crt`|Sender's public key in x509 certificate format, required for *signing*|
 |`sender@example.com.pem`|Sender's private key required for *signing*|
 
-The following environment variable needs to be set: `SPARKPOST_API_KEY`
-
-### Example
-
+### Default text output
+If the `--send_api` option is absent, RFC822 output text is written to the console.
 ```
 $ ./sparkpostSMIME.py testcases/img_and_attachment.eml --sign --encrypt
+To: Bob <bob.lumreeker@gmail.com>
+From: Steve <steve@thetucks.com>
+Subject: Testing attachments etc
+Content-Language: en-GB
+MIME-Version: 1.0
+Content-Type: application/pkcs7-mime; smime-type=enveloped-data; name=smime.p7m
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=smime.p7m
+
+MIMPPccGCSqGSIb3DQEHA6CDDz23MIMPPbICAQAxggHHMIIBwwIBADCBrDCBlzEL
+MAkGA1UEBhMCR0IxGzAZBgNVBAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UE
+:
+:
+```
+
+### Sending via SparkPost API
+If `--send_api` option is present, environment variables are used to send the email via SparkPost.
+
+|Variable|Meaning|
+|---|---|
+|`SPARKPOST_API_KEY`|Required|
+|`SPARKPOST_HOST`|For EU customers set to `https://api.eu.sparkpost.com`. Enterprise customers set to own specific host address|
+
+Example:
+```
+$ ./sparkpostSMIME.py testcases/img_and_attachment.eml --sign --encrypt --send_api
 Opened connection to https://api.sparkpost.com/api/v1
-Sending testcases/img_and_attachment.eml        From: steve@thetucks.com        To: bob.lumreeker@gmail.com     OK - in 3.664 seconds
+OK - in 3.921 seconds
 ```
 
 # mimeshow
-
 Display internal header and MIME structure of a mail file in RFC822 format, indented for ease of reading.
 
 ## Usage 
-
 ```
 $ ./mimeshow.py -h
 usage: mimeshow.py [-h] file
@@ -67,7 +91,6 @@ optional arguments:
 ```
 
 ### Example
-
 ```
 $ ./mimeshow.py testcases/img_and_attachment.eml 
 To Bob <bob.lumreeker@gmail.com>
@@ -123,3 +146,10 @@ export SPARKPOST_API_KEY=<<<YOUR API KEY HERE>>>
 ```
 
 Then create your keys and send .eml files as above.
+
+#### Internal implementation notes
+
+The `Pipfile` currently declares a couple of specific library dependencies for the sparkpostSMIME tool:
+- a [modified fork](https://github.com/tuck1s/python-smime) of the `python-smime` library
+- a pre-release [fork](https://github.com/tuck1s/python-sparkpost) of the `python-sparkpost` library
+
--- sparkpostSMIME.py
+++ sparkpostSMIME.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from __future__ import print_function
-import email, os, time, argparse, smime, base64
+import email, os, time, argparse, smime, base64, sys
 from sparkpost import SparkPost
 from sparkpost.exceptions import SparkPostAPIException
 from email.utils import parseaddr
@@ -68,6 +68,7 @@ def create_embedded_pkcs7_signature(data, cert, key):
 
     return signed_data
 
+
 def signEmailFrom(msg, fromAddr):
     """ Signs the provided email message object with the from address cert (.crt) & private key (.pem) from current dir.
     Returns signed message object, in format
@@ -130,68 +131,52 @@ def copyPayload(m1, m2):
             m2.add_header(i[0], i[1])
 
 
-def sendEml(sp, emlfile, encrypt=False, sign=False):
-    """ Inject into a SparkPost endpoint, given a message file in RFC822 .eml format and recipient public key. """
-    with open(emlfile) as fp:
-        msgIn = email.message_from_file(fp)
-        allRecips = gatherAllRecips(msgIn)
-        _, fromAddr = parseaddr(msgIn.get('From'))
-        _, toAddr = parseaddr(msgIn.get('To'))
-        print('Sending {}\tFrom: {}\tTo: {}\t'.format(emlfile, fromAddr, toAddr), end='')
-        copyPayload(fixTextPlainParts(msgIn), msgIn)
+def buildSMIMEemail(msg, encrypt=False, sign=False):
+    """ Build SMIME email, given a message file in RFC822 .eml format. Options to encrypt and sign. """
+    _, fromAddr = parseaddr(msg.get('From'))
+    _, toAddr = parseaddr(msg.get('To'))
+    copyPayload(fixTextPlainParts(msg), msg)
+    # Sign the message, replacing it in-situ
+    if sign:
+        msg = signEmailFrom(msg, fromAddr)
 
-        # Sign the message
-        if sign:
-            msgIn = signEmailFrom(msgIn, fromAddr)
+    # Encrypt the message, replacing it in-situ
+    if encrypt:
+        rcptFile = toAddr + '.crt'
+        with open(rcptFile, 'rb') as crtFile:
+            rcptPem = crtFile.read()
+            msg = smime.encrypt(msg, rcptPem)
+    return msg
 
-        # Encrypt the message, returning as a string
-        if encrypt:
-            rcptFile = toAddr + '.crt'
-            with open(rcptFile, 'rb') as crtFile:
-                rcptPem = crtFile.read()
-                s = smime.encrypt(msgIn, rcptPem)
+
+def sendSparkPost(sp, msg):
+    """ Inject into a SparkPost endpoint, given a message object """
+    allRecips = gatherAllRecips(msg)
+    # Prevent SparkPost from wrapping links and inserting tracking pixels, if signed or encrypted.
+    # Also set "transactional" flag to suppress the List-Unsubscribe header.
+    sendObj = {
+        'campaign': 'sparkpost-SMIME',
+        'track_opens': False,
+        'track_clicks': False,
+        'transactional': True,
+        'email_rfc822': msg.as_string(),
+        'recipients': allRecips
+    }
+    # send message via SparkPost
+    startT = time.time()
+    try:
+        res = sp.transmissions.send(**sendObj)                  # Unpack for the call
+        endT = time.time()
+        if res['total_accepted_recipients'] != len(sendObj['recipients']):
+            print(res)
         else:
-            s = msgIn.as_string()
+            print('OK - in', round(endT - startT, 3), 'seconds')
+        return res['total_accepted_recipients'], ''
+    except SparkPostAPIException as err:
+        errMsg = 'error code ' + str(err.status) + ' : ' + str(err.errors)
+        print(errMsg)
+        return 0, errMsg
 
-        # debug code for output of mail to a file
-        #with open('debug.eml', 'w') as f:
-        #    f.write(s)
-        #    f.close()
-
-        # Prevent SparkPost from wrapping links and inserting tracking pixels, if signed or encrypted.
-        # Also set "transactional" flag to suppress the List-Unsubscribe header.
-        canTrack = not (sign or encrypt)
-        sendObj = {
-            'campaign': 'sparkpost-SMIME',
-            'track_opens': canTrack,
-            'track_clicks': canTrack,
-            'transactional': True,
-            'email_rfc822': s,
-            'recipients': allRecips
-        }
-
-        # send message via SparkPost
-        startT = time.time()
-        try:
-            res = sp.transmissions.send(**sendObj)                  # Unpack for the call
-            endT = time.time()
-            if res['total_accepted_recipients'] != len(sendObj['recipients']):
-                print(res)
-            else:
-                print('OK - in', round(endT - startT, 3), 'seconds')
-            return res['total_accepted_recipients'], ''
-        except SparkPostAPIException as err:
-            errMsg = 'error code ' + str(err.status) + ' : ' + str(err.errors)
-            print(errMsg)
-            return 0, errMsg
-
-
-def testCases(sp):
-    """ Test code only """
-    for fname in os.listdir('testcases'):
-        for enc in [False, True]:
-            for sign in [False, True]:
-                sendEml(sp, os.path.join('testcases',fname), encrypt=enc, sign=sign)
 
 # -----------------------------------------------------------------------------------------
 # Main code
@@ -201,14 +186,25 @@ if __name__ == "__main__":
     parser.add_argument('emlfile', type=str, help='filename to read (in RFC822 format)')
     parser.add_argument('--encrypt', action='store_true', help='Encrypt with a recipient certificate containing public key. Requires file.crt where file matches To: address.')
     parser.add_argument('--sign', action='store_true', help='Sign with a sender key. Requires file.crt containing public key, and file.pem containing private key, where file matches From: address.')
-    args = parser.parse_args()
-    cfg = getConfig()
+    parser.add_argument('--send_api', action='store_true', help='Send via SparkPost API (using env var SPARKPOST_API_KEY).')
 
+    args = parser.parse_args()
     if os.path.isfile(args.emlfile):
-        sp = SparkPost(api_key=cfg['sparkpost_api_key'], base_uri=cfg['sparkpost_host'])
-        print('Opened connection to', sp.base_uri)
-        #testCases(sp)               # Used for internal testing
-        sendEml(sp, args.emlfile, encrypt=args.encrypt, sign=args.sign)
+        with open(args.emlfile) as fp:
+            msgOut = buildSMIMEemail(email.message_from_file(fp), encrypt=args.encrypt, sign=args.sign)
+            if args.send_api:
+                cfg = getConfig()
+                sp = SparkPost(api_key=cfg['sparkpost_api_key'], base_uri=cfg['sparkpost_host'])
+                print('Opened connection to', sp.base_uri)
+                sendSparkPost(sp, msgOut)
+            else:
+                try:
+                    print(msgOut.as_string())
+                except BrokenPipeError:
+                    # See https://docs.python.org/3/library/signal.html#note-on-sigpipe
+                    devnull = os.open(os.devnull, os.O_WRONLY)
+                    os.dup2(devnull, sys.stdout.fileno())
+                    sys.exit(1)  # Python exits with error code 1 on EPIPE
     else:
         print('Unable to open file', args.emlfile)
         exit(1)
